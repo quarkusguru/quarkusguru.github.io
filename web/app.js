676af9d778e435736bfe2f241414cb83763172ab
Matheus Cruz <56329339+mcruzdev@users.noreply.github.com>
2026-05-18 01:34:24 -0300

    Understanding @Transactional in Quarkus (#6)
    * Add Understanding @Transactional in Quarkus
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>
    
    * Change medium-zoom scope to provided
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>
    
    ---------
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

--- web/app.js
+++ web/app.js
@@ -1,5 +1,8 @@
 import "@fontsource/atkinson-hyperlegible";
 import hljs from 'highlight.js';
 import 'highlight.js/styles/androidstudio.min.css';
+import mediumZoom from 'medium-zoom';
 
 hljs.highlightAll();
+
+mediumZoom('img');
